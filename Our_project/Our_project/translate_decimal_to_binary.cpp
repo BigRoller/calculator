ccb9f22ac26f1853cb0aaf9c7865816c50e98c2c
gleb29lvl <154795779+gleb29lvl@users.noreply.github.com>
2023-12-27 15:40:18 +0300

    Update translate_decimal_to_binary.cpp

--- Our_project/Our_project/translate_decimal_to_binary.cpp
+++ Our_project/Our_project/translate_decimal_to_binary.cpp
@@ -3,10 +3,10 @@
 #include "translate_decimal_to_binary.h"
 using namespace std;
 
-// Функция для перевода чисел из 10 в 2 систему счисления
+
 string translateDecimalToBinary(int num) {
   string result;
-  while (num > 0) { // Пока число не равно нулю, делим его на 2 и добавляем остаток к результату
+  while (num > 0) { 
     result = char('0' + num % 2) + result;
     num /= 2;
   }
